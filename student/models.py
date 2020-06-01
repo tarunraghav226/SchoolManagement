82972319b75ad291204a11d81a1743441a266d1e
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-01 13:51:47 +0530

    Added student model and registered it.

--- student/models.py
+++ student/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    admission_number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    class_of_student = models.CharField(max_length=10)
+    roll_number = models.IntegerField()
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table = 'Student'
+
+    def __str__(self):
+        return self.admission_number
