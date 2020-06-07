a75e61057de2dc53cd57903d52d35890c18cf047
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-07 19:33:31 +0530

    Added Teacher and Teaches model and registered them.

--- teacher/admin.py
+++ teacher/admin.py
@@ -1,1 +1,7 @@
 # Register your models here.
+from django.contrib import admin
+
+from teacher.models import Teacher, Teaches
+
+admin.site.register(Teacher)
+admin.site.register(Teaches)
--- teacher/models.py
+++ teacher/models.py
@@ -1,1 +1,26 @@
 # Create your models here.
+from django.db import models
+
+
+class Teacher(models.Model):
+    teacher_id = models.CharField(max_length=10, primary_key=True)
+    teacher_name = models.CharField(max_length=100)
+    class_teacher_of = models.CharField(max_length=10)
+    teacher_image = models.ImageField(upload_to='teacher', default='user.jpeg')
+
+    class Meta:
+        db_table = 'Teacher'
+
+    def __str__(self):
+        return self.teacher_name + ' ' + self.id
+
+
+class Teaches(models.Model):
+    teacher_id = models.CharField(max_length=10)
+    subject_code = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'Teaches'
+
+    def __str__(self):
+        return self.id + ' ' + self.subject_code
