48e8675ec54d5f4355836b2b419dda89efb955c6
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-13 17:45:23 +0530

    Updated models of teacher.

--- teacher/models.py
+++ teacher/models.py
@@ -2,17 +2,21 @@
 from django.db import models
 
 
+def user_directory_path(instance, filename):
+    return 'teacher/{0}/{1}'.format(instance.teacher_id, instance.teacher_name + filename)
+
+
 class Teacher(models.Model):
     teacher_id = models.CharField(max_length=10, primary_key=True)
     teacher_name = models.CharField(max_length=100)
-    class_teacher_of = models.CharField(max_length=10)
-    teacher_image = models.ImageField(upload_to='teacher', default='user.jpeg')
+    class_teacher_of = models.CharField(max_length=10, default='none')
+    teacher_image = models.ImageField(upload_to=user_directory_path, default='user.jpeg')
 
     class Meta:
         db_table = 'Teacher'
 
     def __str__(self):
-        return self.teacher_name + ' ' + self.id
+        return self.teacher_name + ' ' + self.teacher_id
 
 
 class Teaches(models.Model):
@@ -23,4 +27,4 @@ class Teaches(models.Model):
         db_table = 'Teaches'
 
     def __str__(self):
-        return self.id + ' ' + self.subject_code
+        return self.teacher_id + ' ' + self.subject_code
