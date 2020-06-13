5665fc23bd941b94c8aca1919d2b7501f6fcb293
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-13 19:42:40 +0530

    Completed functionality of add student.

--- teacher/views.py
+++ teacher/views.py
@@ -1,1 +1,61 @@
 # Create your views here.
+from django.contrib import messages
+from django.shortcuts import render
+
+from student.models import Student, Subjects
+from teacher.forms import AddStudentForm
+from teacher.models import Teacher, Teaches
+
+
+def home(request):
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    return render(request, 'teacher/home-dashboard.html', context)
+
+
+def analytic_dashboard(request):
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    if context['user_detail'].class_teacher_of != 'none':
+        context['students_in_class'] = Student.objects.filter(class_of_student=context['user_detail'].class_teacher_of)
+    return render(request, 'teacher/analytic-dashboard.html', context)
+
+
+def details(request):
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    subject_list = []
+    for value in Teaches.objects.filter(teacher_id=request.user.username):
+        subject_list.append(Subjects.objects.get(subject_code=value.subject_code).subject_name)
+    context['teaches'] = subject_list
+    return render(request, 'teacher/details-dashboard.html', context)
+
+
+def add(request):
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    context['form'] = AddStudentForm()
+    return render(request, 'teacher/add-student.html', context)
+
+
+def search(request):
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    return render(request, 'teacher/search-dashboard.html', context)
+
+
+def add_new_student(request):
+    form = AddStudentForm(request.POST, request.FILES)
+    context = {}
+    context['user_detail'] = Teacher.objects.get(teacher_id=request.user.username)
+    context['form'] = form
+    if request.method == 'POST':
+        if form.is_valid():
+            messages.info(request, "Student (" + str(form.cleaned_data['admission_number']) + ") is added.")
+            form.save()
+            context['form'] = AddStudentForm()
+            return render(request, 'teacher/add-student.html', context)
+    else:
+        messages.info(request, 'Student Admission Number - Not added')
+        return render(request, 'teacher/add-student.html', context)
+    return render(request, 'teacher/add-student.html', context)
