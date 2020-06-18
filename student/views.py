d14bb1914da2cae35b0a5c1a485ed8c75b496afb
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-18 20:15:17 +0530

    Updated Search feature of student.

--- student/views.py
+++ student/views.py
@@ -2,24 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from student.models import Student, Marks, Subjects
+from teacher.models import Teacher, Teaches
 
 
 def dashboard(request):
-    context = {}
-    context['user_detail'] = Student.objects.get(admission_number=request.user.username)
+    context = {'user_detail': Student.objects.get(admission_number=request.user.username)}
     return render(request, 'student/home-dashboard.html', context)
 
 
 def analytic_dashboard(request):
-    context = {}
-    context['user_detail'] = Student.objects.get(admission_number=request.user.username)
-    context['subjects'] = Marks.objects.filter(admission_number=request.user.username)
+    context = {'user_detail': Student.objects.get(admission_number=request.user.username),
+               'subjects': Marks.objects.filter(admission_number=request.user.username)}
     return render(request, 'student/analytic-dashboard.html', context)
 
 
 def details(request):
-    context = {}
-    context['user_detail'] = Student.objects.get(admission_number=request.user.username)
+    context = {'user_detail': Student.objects.get(admission_number=request.user.username)}
     subject_name = []
     for i in Marks.objects.filter(admission_number=request.user.username):
         subject_name.append(Subjects.objects.get(subject_code=i.subject_code).subject_name)
@@ -28,27 +26,32 @@ def details(request):
 
 
 def search(request):
-    context = {}
-    context['user_detail'] = Student.objects.get(admission_number=request.user.username)
-    context['search'] = ''
+    context = {'user_detail': Student.objects.get(admission_number=request.user.username), 'search': ''}
     return render(request, 'student/search-dashboard.html', context)
 
 
 def get_result(request):
-    context = {}
-    context['search'] = ''
-    context['user_detail'] = Student.objects.get(admission_number=request.user.username)
+    context = {'search': '', 'user_detail': Student.objects.get(admission_number=request.user.username)}
     try:
-        object = Student.objects.get(admission_number=request.GET.get(key='id'))
-        subject_name = []
-        for i in Marks.objects.filter(admission_number=request.GET.get(key='id')):
-            subject_name.append(Subjects.objects.get(subject_code=i.subject_code).subject_name)
-        context['subjects'] = subject_name
-        context['search'] = object
+        if request.GET.get('search_whom') == 'Student':
+            context['searched_user'] = 'Student'
+            student_object = Student.objects.get(admission_number=request.GET.get(key='id'))
+            subject_name = []
+            for i in Marks.objects.filter(admission_number=request.GET.get(key='id')):
+                subject_name.append(Subjects.objects.get(subject_code=i.subject_code).subject_name)
+            context['subjects'] = subject_name
+            context['search'] = student_object
+        else:
+            context['searched_user'] = 'Teacher'
+            teacher_object = Teacher.objects.get(teacher_id=request.GET.get('id'))
+            subject_name = []
+            for subject in Teaches.objects.filter(teacher_id=request.GET.get('id')):
+                subject_name.append(Subjects.objects.get(subject_code=subject.subject_code).subject_name)
+            print(subject_name)
+            context['subjects'] = subject_name if subject_name != [] else 'none'
+
+            context['search'] = teacher_object
     except Exception:
         context['search'] = "user_not_found_404"
     return render(request, 'student/search-dashboard.html', context)
 
-
-def add(request):
-    return render(request, 'head/add-teacher.html')
