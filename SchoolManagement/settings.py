5c0c22f736657edfa668f9473b4ba0eaaecea9ba
tarunraghav226 <tarun.raghav226@gmail.com>
2020-06-21 15:49:18 +0530

    Updated Settings file.

--- SchoolManagement/settings.py
+++ SchoolManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*oury&a$*v8kp_wgr!97&7pmh&^uir*n*2a_k6jcnn4usvl+fu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'allstatic')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
