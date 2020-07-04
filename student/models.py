45c7f9011930eb03f7ce4bc36894f3b4b04a54a2
tarunraghav226 <tarun.raghav226@gmail.com>
2020-07-04 18:59:26 +0530

    Added update signal for marks model.

--- student/models.py
+++ student/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+from django.dispatch import receiver
 
 
 class Student(models.Model):
@@ -54,3 +55,24 @@ class Marks(models.Model):
 
     def __str__(self):
         return str(self.admission_number) + " " + self.subject_code
+
+
+@receiver(post_save, sender=Marks)
+def update_marks(sender, instance, created, **kwargs):
+    if not created:
+        subject_list = Marks.objects.filter(admission_number=instance.admission_number)
+        student = Student.objects.get(admission_number=instance.admission_number)
+        student.percentage_of_mid1 = calculate_percentage([subject.mid1 for subject in subject_list], 'm')
+        student.percentage_of_mid2 = calculate_percentage([subject.mid2 for subject in subject_list], 'm')
+        student.percentage_of_final = calculate_percentage([subject.final for subject in subject_list], 'f')
+        student.save()
+
+
+def calculate_percentage(list_of_subject, term):
+    mul = 1
+    if term == 'm':
+        mul = 10
+    percentage = 0
+    for marks in list_of_subject:
+        percentage += marks
+    return (percentage * mul) / len(list_of_subject)
